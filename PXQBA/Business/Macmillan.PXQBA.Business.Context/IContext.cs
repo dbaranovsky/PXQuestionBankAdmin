b6424a211081f20baf5fc6246f379ba18e999630
VKisel <you@example.co>
2014-03-10 20:39:48 +0300

    QBA-37: Business Logic Implementation: add GetQuestionList method implementation with hardcoded EntityId

--- PXQBA/Business/Macmillan.PXQBA.Business.Context/IContext.cs
+++ PXQBA/Business/Macmillan.PXQBA.Business.Context/IContext.cs
@@ -1,9 +1,11 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using Bfw.Agilix.Dlap.Session;
 
 namespace Macmillan.PXQBA.Business
 {
     public interface IContext
     {
         void InitializeSession(string userName);
+        ISessionManager SessionManager { get; }
     }
 }
--- PXQBA/Business/Macmillan.PXQBA.Business.Contracts/IQuestionListManagementService.cs
+++ PXQBA/Business/Macmillan.PXQBA.Business.Contracts/IQuestionListManagementService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bfw.Agilix.DataContracts;
 
 namespace Macmillan.PXQBA.Business.Contracts
 {
     public interface IQuestionListManagementService
     {
+        /// <summary>
+        /// Retrieves questions list
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Question> GetQuestionList();
     }
 }
