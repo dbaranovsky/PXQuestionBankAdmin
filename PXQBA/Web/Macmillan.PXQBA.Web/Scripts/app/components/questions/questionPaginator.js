2cc13a5a51d5c6fb64c88cf1301f98e788df3099
dbazylev <d.bazylev@gmail.com>
2014-03-12 20:53:50 +0300

    qba-69: Implement question paginator UI

--- PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/questions/questionPaginator.js
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/questions/questionPaginator.js
@@ -3,10 +3,22 @@
 */ 
 
 var QuestionPaginator = React.createClass({displayName: 'QuestionPaginator',
+
+	componentDidMount: function() {
+         var options = {
+            currentPage: 1,
+            totalPages: 10,
+            onPageChanged: function(event, oldPage, newPage) {
+            	//ToDo: Implement 
+            }
+        }
+
+        $('#question-paginator').bootstrapPaginator(options);
+	},
+
 	render: function() {
 		return ( 
-			   React.DOM.div( {className:"questionPaginator"},  
-			   		React.DOM.span(null, "Paging")
+			   React.DOM.div( {className:"questionPaginator", id:"question-paginator"} 
 			   )
 			);
 		}
