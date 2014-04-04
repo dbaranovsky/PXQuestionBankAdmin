09016c3c24cccb3c3c067c921b069eb266c1df60
dbazylev <d.bazylev@gmail.com>
2014-04-04 13:09:38 +0300

    + add layout main menu, return login page

---
+++ PXQBA/Web/Macmillan.PXQBA.Web/Controllers/ImportController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Macmillan.PXQBA.Web.Controllers
+{
+    public class ImportController : MasterController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
