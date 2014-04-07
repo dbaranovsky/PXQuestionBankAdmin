365645cc5cf202ed97336ef24fbf20e26cd44bf9
Aleksey Yulin <ulin@tut.by>
2014-04-06 21:47:07 -0500

    * Question metafield model definition

---
+++ PXQBA/Business/Macmillan.PXQBA.Business.Models/MetadataFieldNames.cs
@@ -0,0 +1,16 @@
+﻿namespace Macmillan.PXQBA.Business.Models
+{
+    public static class MetadataFieldNames
+    {
+        public const string DlapStatus = "dlap_q_status";
+
+        public const string InlinePreview = "questionHtmlInlinePreview";
+        public const string DlapType = "dlap_q_type";
+        public const string DlapTitle = "dlap_title";
+        public const string Id = "id";
+        public const string Chapter = "chapter";
+        public const string Bank = "bank";
+        public const string Sequence = "seq";
+        public const string Difficulty = "difficulty";
+    }
+}
