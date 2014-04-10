2fb5a3e8fbe6d4e6d461fe10768514ff7c6c9644
dbazylev <d.bazylev@gmail.com>
2014-04-10 15:22:08 +0300

    ! qba-102 It is possible to open in-line edit for many fields at once

--- PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/questions/questionInlineEditorBase.js
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/questions/questionInlineEditorBase.js
@@ -4,6 +4,20 @@
 
 var QuestionInlineEditorBase = React.createClass({displayName: 'QuestionInlineEditorBase',
     
+    componentDidMount: function() {
+        $(document).click(this.clickOutSideControlHandler);
+    },
+
+    componentWillUnmount: function() {
+        $(document).unbind( "click", this.clickOutSideControlHandler);
+    },
+
+    clickOutSideControlHandler: function(event) {
+        if($(event.target).parents().index($('.inline-editor-container')) == -1) {
+               this.props.afterEditingHandler();
+        }
+    },
+
     saveVelueHandler: function(value) {
         if(value != null) {
           questionDataManager.saveQuestionData(this.props.metadata.questionId,
@@ -36,7 +50,7 @@ var QuestionInlineEditorBase = React.createClass({displayName: 'QuestionInlineEd
 
      render: function() {
         return ( 
-            React.DOM.div(null, 
+            React.DOM.div( {className:"inline-editor-container"}, 
                this.renderSpecificEditor()
             )
             );
