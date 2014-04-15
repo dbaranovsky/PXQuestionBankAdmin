183ee44382bc1898199534a4a0c63dcb4b069e0b
VKisel <you@example.co>
2014-04-15 18:25:40 +0300

    * question editor refactoring

---
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/questions/editor/questionEditor.js
@@ -0,0 +1,49 @@
+/**
+* @jsx React.DOM
+*/
+var QuestionEditor = React.createClass({displayName: 'QuestionEditor',
+
+   getInitialState: function() {
+
+      return { question: this.props.question };
+    },
+
+
+    saveQuestion: function(){
+       // if(this.props.isNew)
+       // {
+       // }
+        var finishSaving = this.props.finishSaving;
+        questionDataManager.updateQuestion(this.state.question).done(finishSaving);
+
+    },
+
+    editHandler: function(editedQuestion){
+      this.setState({question: editedQuestion});
+    },
+
+    componentDidMount: function(){
+       
+    },
+
+    render: function() {
+        return (
+            React.DOM.div(null, 
+                      React.DOM.div( {className:"header-buttons"}, 
+                         React.DOM.button( {className:"btn btn-primary run-question", 'data-toggle':"modal"} , 
+                             React.DOM.span( {className:"glyphicon glyphicon-play"}), " Run Question"
+                        ),
+                        React.DOM.button( {className:"btn btn-default", 'data-toggle':"modal", onClick:this.props.closeDialog}, 
+                             "Cancel"
+                        ),
+                         React.DOM.button( {className:"btn btn-primary ",  'data-toggle':"modal", onClick:this.saveQuestion} , 
+                             "Save"
+                        )
+                      ),
+                
+                React.DOM.div(null, 
+                  QuestionEditorTabs( {question:this.state.question, editHandler:this.editHandler} )
+                )
+         ));
+    }
+});
---
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/src/components/questions/editor/multiSelectEditor.js
@@ -0,0 +1,89 @@
+/**
+* @jsx React.DOM
+*/
+var MultiSelectEditor = React.createClass({
+
+ 
+    getInitialState: function(){
+         var metadataValues = [];
+         var currentValues = this.props.question[this.props.field];
+     
+        var  availableChoices =  this.props.metadataField.editorDescriptor.availableChoice;
+
+        for (var propertyName in availableChoices) {
+            availableChoice = availableChoices[propertyName];
+            metadataValues.push(availableChoice);
+        }
+
+        if(currentValues !== undefined && currentValues != null && currentValues.length>0){
+          $.merge(metadataValues, currentValues);
+        }
+      
+        metadataValues = this.unique(metadataValues);
+        var options = [];
+         $.each(metadataValues, function(i, option){
+               options.push(<option value={option}>{option}</option>);
+         });
+
+         return ({options: options})
+
+    },
+
+    unique: function(list) {
+        var result = [];
+        $.each(list, function(i, e) {
+            if ($.inArray(e, result) == -1) result.push(e);
+        });
+        return result;
+    },
+
+
+     editHandler: function(selectedOptions){
+      
+       
+       var items = [];
+       
+       $.each(selectedOptions, function(i, option){
+          items.push(option.text);
+       });
+      
+
+      var question = this.props.question;
+      if (question[this.props.field]== null || question[this.props.field].length !== items.length)
+      {
+        question[this.props.field] = items;
+        this.props.editHandler(question);
+      }
+     
+
+     },
+
+    renderMenuItems: function() {
+
+        return (this.state.options);
+    },
+
+
+    componentDidMount: function(){
+        var selector = this.getDOMNode();
+        var chosenOptions = {width: "100%", hide_dropdown: false};
+        var handler =  this.editHandler;
+        $(selector).val(this.props.question[this.props.field])
+                          .chosen(chosenOptions)
+                          .change(function(e, params){
+                             handler(e.currentTarget.selectedOptions);
+                              setTimeout(function(){ $(selector).trigger('chosen:open'); }, 0)
+                           });
+
+                         
+    },
+
+   
+    render: function() {
+        return (
+             <select data-placeholder="No Value" multiple>
+                    {this.renderMenuItems()}  
+             </select> 
+         );
+    }
+});
