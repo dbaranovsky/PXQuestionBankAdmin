c794a78d91e22c3df7d16280d61effc726509b08
dbazylev <d.bazylev@gmail.com>
2014-04-28 20:42:13 +0300

    increase build version

--- PXQBA/Web/Macmillan.PXQBA.Web/Properties/ProductInfo.cs
+++ PXQBA/Web/Macmillan.PXQBA.Web/Properties/ProductInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.10.*")]
-[assembly: AssemblyFileVersion("1.0.10.*")]
+[assembly: AssemblyVersion("1.0.11.*")]
+[assembly: AssemblyFileVersion("1.0.11.*")]
