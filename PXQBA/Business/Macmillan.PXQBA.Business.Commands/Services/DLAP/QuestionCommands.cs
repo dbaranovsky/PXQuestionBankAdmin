57dc2c7325fae586a057f8295cdcd77666c0cca6
dbazylev <d.bazylev@gmail.com>
2014-05-20 15:37:29 +0300

    ! qba-143: fix fail grid after bad filtration

--- PXQBA/Business/Macmillan.PXQBA.Business.Commands/Services/DLAP/QuestionCommands.cs
+++ PXQBA/Business/Macmillan.PXQBA.Business.Commands/Services/DLAP/QuestionCommands.cs
@@ -115,6 +115,10 @@ namespace Macmillan.PXQBA.Business.Commands.Services.DLAP
         private IEnumerable<Bfw.Agilix.DataContracts.Question> GetAgilixQuestions(string repositoryCourseId,
             IEnumerable<string> questionIds)
         {
+            if (!questionIds.Any())
+            {
+                return new List<Bfw.Agilix.DataContracts.Question>();
+            }
             var cmd = new GetQuestions()
             {
                 SearchParameters = new QuestionSearch()
