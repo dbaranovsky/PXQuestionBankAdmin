59fb9a0e7a2fa8b5ee7ff01a4f34b1f564f652c0
dbaranovsky <dbaranovsky88@gmail.com>
2014-05-23 16:03:28 -0400

    ! fix build

--- PXQBA/Web/Macmillan.PXQBA.Web/Properties/ProductInfo.cs
+++ PXQBA/Web/Macmillan.PXQBA.Web/Properties/ProductInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Macmillan 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+   
 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.17.*")]
