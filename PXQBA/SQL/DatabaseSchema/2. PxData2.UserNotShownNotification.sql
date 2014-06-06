5887f6206f9220fc9d7ff9b4bb903656620fbd00
dbaranovsky <dbaranovsky88@gmail.com>
2014-06-06 15:13:20 +0300

    ! missed file

---
+++ PXQBA/SQL/DatabaseSchema/2. PxData2.UserNotShownNotification.sql
@@ -0,0 +1,51 @@
+USE PXData2
+GO
+
+CREATE TABLE dbo.UserNotShownNotification(
+  Id BIGINT IDENTITY,
+  NotificationType NVARCHAR(MAX) NOT NULL,
+
+  UserId NVARCHAR(MAX) NOT NULL,
+  CONSTRAINT PK_QBANotes PRIMARY KEY (Id)
+) 
+GO
+
+USE PXData2
+GO
+
+CREATE PROCEDURE dbo.GetUserNotShownNotifications
+(
+  @userId NVARCHAR(MAX)
+)
+AS
+BEGIN
+  SELECT *
+  FROM
+    UserNotShownNotification
+  WHERE
+    UserId = @userId
+END
+GO
+
+
+
+USE PXData2
+GO
+
+CREATE PROCEDURE dbo.CreateUserNotShownNotification
+(
+  @userId   NVARCHAR(MAX),
+  @notificationType NVARCHAR(MAX),
+)
+AS
+BEGIN
+  
+IF NOT EXISTS (SELECT * FROM UserNotShownNotification n WHERE n.UserId=@userId AND n.NotificationType = @notificationType)
+BEGIN
+  INSERT INTO UserNotShownNotification(UserId, NotificationType)
+  VALUES
+    (@userId, @notificationType)
+END
+ 
+END
+GO
