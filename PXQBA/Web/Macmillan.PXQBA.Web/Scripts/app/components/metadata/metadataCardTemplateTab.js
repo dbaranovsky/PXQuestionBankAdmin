cb2747051930b5c55706c7b3d1bf9159f2c879b5
dbazylev <d.bazylev@gmail.com>
2014-06-18 20:05:06 +0300

    + qba-6: implement card preview

--- PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/metadata/metadataCardTemplateTab.js
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/components/metadata/metadataCardTemplateTab.js
@@ -6,8 +6,49 @@ var MetadataCardTemplateTab = React.createClass({displayName: 'MetadataCardTempl
 
   cardTemplateName: "questionCardLayout",
 
+
+  getInitialState: function() {
+        return {
+          showPreviewCardTemplateDialog: false,
+          cardHtml: ""
+        };
+  },
+
+  createDummyObject: function() {
+      var dummyObject = {};
+      var curlyBracesPattern = /[^{}]+(?=\})/g;
+      var results = this.props.data[this.cardTemplateName].match(curlyBracesPattern);
+
+      var charactersPattern = /^\w+$/;
+      var properties=[];
+
+      for(var i=0; i<results.length; i++) {
+        if(charactersPattern.test(results[i])) {
+          properties.push(results[i]);
+        }
+      }
+
+      for(var i=0; i<properties.length; i++) {
+        dummyObject[properties[i]]=properties[i];
+      }
+
+      return dummyObject;
+  },
+
   runPreview: function() {
-      alert('run!');
+     try {
+         var dummyObject = this.createDummyObject();
+         var template = Handlebars.compile(this.props.data[this.cardTemplateName]);
+         var cardHtml = template(dummyObject);
+          
+         this.setState({
+              showPreviewCardTemplateDialog: true,
+              cardHtml: cardHtml
+         });
+     }
+     catch (exception) {
+        notificationManager.showDanger("There is a syntax error on template: " +exception.message)
+     }
   },
 
 
@@ -17,9 +58,29 @@ var MetadataCardTemplateTab = React.createClass({displayName: 'MetadataCardTempl
       this.props.dataChangeHandler(data);
   },
 
+  closePreviewCardTemplateDialogHandler: function() {
+     this.setState({
+                showPreviewCardTemplateDialog: false,
+                cardHtml: ""
+      });
+  },
+
+  renderPreviewCardTemplateDialog: function() {
+    if(this.state.showPreviewCardTemplateDialog) {
+      return (PreviewCardTemplateDialog( {closeDialogHandler:this.closePreviewCardTemplateDialogHandler, 
+                                  cardHtml:this.state.cardHtml} 
+                                  ));
+    }
+
+    return null;
+  },
+
   render: function() {
        return (
          React.DOM.div(null, 
+             React.DOM.div(null, 
+              this.renderPreviewCardTemplateDialog()
+             ),
              React.DOM.div( {className:"metadata-template-header"}, 
                   React.DOM.div( {className:"metadata-template-description"}, 
                       React.DOM.span(null,  " Edit question card HTML code Here " ),
--- PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/src/components/metadata/metadataCardTemplateTab.js
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/src/components/metadata/metadataCardTemplateTab.js
@@ -6,8 +6,49 @@ var MetadataCardTemplateTab = React.createClass({
 
   cardTemplateName: "questionCardLayout",
 
+
+  getInitialState: function() {
+        return {
+          showPreviewCardTemplateDialog: false,
+          cardHtml: ""
+        };
+  },
+
+  createDummyObject: function() {
+      var dummyObject = {};
+      var curlyBracesPattern = /[^{}]+(?=\})/g;
+      var results = this.props.data[this.cardTemplateName].match(curlyBracesPattern);
+
+      var charactersPattern = /^\w+$/;
+      var properties=[];
+
+      for(var i=0; i<results.length; i++) {
+        if(charactersPattern.test(results[i])) {
+          properties.push(results[i]);
+        }
+      }
+
+      for(var i=0; i<properties.length; i++) {
+        dummyObject[properties[i]]=properties[i];
+      }
+
+      return dummyObject;
+  },
+
   runPreview: function() {
-      alert('run!');
+     try {
+         var dummyObject = this.createDummyObject();
+         var template = Handlebars.compile(this.props.data[this.cardTemplateName]);
+         var cardHtml = template(dummyObject);
+          
+         this.setState({
+              showPreviewCardTemplateDialog: true,
+              cardHtml: cardHtml
+         });
+     }
+     catch (exception) {
+        notificationManager.showDanger("There is a syntax error on template: " +exception.message)
+     }
   },
 
 
@@ -17,9 +58,29 @@ var MetadataCardTemplateTab = React.createClass({
       this.props.dataChangeHandler(data);
   },
 
+  closePreviewCardTemplateDialogHandler: function() {
+     this.setState({
+                showPreviewCardTemplateDialog: false,
+                cardHtml: ""
+      });
+  },
+
+  renderPreviewCardTemplateDialog: function() {
+    if(this.state.showPreviewCardTemplateDialog) {
+      return (<PreviewCardTemplateDialog closeDialogHandler={this.closePreviewCardTemplateDialogHandler} 
+                                  cardHtml={this.state.cardHtml} 
+                                  />);
+    }
+
+    return null;
+  },
+
   render: function() {
        return (
          <div>
+             <div>
+              {this.renderPreviewCardTemplateDialog()}
+             </div>
              <div className="metadata-template-header">
                   <div className="metadata-template-description">
                       <span> Edit question card HTML code Here </span>
