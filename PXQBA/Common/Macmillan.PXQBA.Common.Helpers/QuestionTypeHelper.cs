9bd029673b9c461c66adabc2cec428fb371f16d6
dbazylev <d.bazylev@gmail.com>
2014-06-20 12:48:58 +0300

    ! fix build

--- PXQBA/Common/Macmillan.PXQBA.Common.Helpers/QuestionTypeHelper.cs
+++ PXQBA/Common/Macmillan.PXQBA.Common.Helpers/QuestionTypeHelper.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Macmillan.PXQBA.Business.Models;
 
 namespace Macmillan.PXQBA.Common.Helpers
 {
