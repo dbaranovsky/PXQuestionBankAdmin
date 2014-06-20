a45d6a65b0acb9a5ea5d277b9a746d13325eca81
dbaranovsky <dbaranovsky88@gmail.com>
2014-06-20 10:35:20 +0300

    ! fix role editing

---
+++ PXQBA/SQLQueries/DummyDataScripts/Clear (User management) - Copy.sql
@@ -0,0 +1,13 @@
+
+drop table QBARoleCapability
+drop table UserQBARole
+drop table QBARole
+
+drop procedure GetQBARolesForCourse
+drop procedure AddQBARole
+drop procedure UpdateQBARole
+drop procedure DeleteQBARole
+drop procedure UpdateQBARoleCapabilities
+drop type dbo.QBACapabilityList
+drop procedure dbo.GetQBARoleCapabilities
+drop procedure dbo.GetUsersForQBA
