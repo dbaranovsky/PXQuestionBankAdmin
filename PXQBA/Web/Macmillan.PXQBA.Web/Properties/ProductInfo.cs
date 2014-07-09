c9ee7353d216166684e690f1b5626a9a6f28892d
VKisel <you@example.co>
2014-07-09 14:15:19 +0300

    *change product info

--- PXQBA/Web/Macmillan.PXQBA.Web/Properties/ProductInfo.cs
+++ PXQBA/Web/Macmillan.PXQBA.Web/Properties/ProductInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
    
 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.29.*")]
-[assembly: AssemblyFileVersion("1.0.29.*")]
+[assembly: AssemblyVersion("1.0.30.*")]
+[assembly: AssemblyFileVersion("1.0.30.*")]
