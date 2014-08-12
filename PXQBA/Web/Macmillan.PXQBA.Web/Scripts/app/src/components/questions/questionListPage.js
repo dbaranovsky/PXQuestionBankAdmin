d01ea6b34e18bb43023e67de2e2497aa1189adbb
dbaranovsky <dbaranovsky88@gmail.com>
2014-08-12 15:32:15 +0300

    ! fix qba-368

--- PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/src/components/questions/questionListPage.js
+++ PXQBA/Web/Macmillan.PXQBA.Web/Scripts/app/src/components/questions/questionListPage.js
@@ -211,7 +211,8 @@ var QuestionListPage = React.createClass({
         return (<EditQuestionNotesDialog closeDialogHandler={this.closeNoteDialogHandler}
                                          questionId={this.state.questionIdForNotes} 
                                          canDelete={this.props.response.canRemoveNotesQuestion}
-                                         canAddNote={this.props.response.canAddNotesQuestion}/>);
+                                         canAddNote={this.props.response.canAddNotesQuestion}
+                                         currentCourseId= {this.props.response.productCourseId} />);
       }
       return null;
     },
