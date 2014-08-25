e6cb09655a586c6b8cfb99cb1d53e1079f81aff2
VKisel <you@example.co>
2014-08-25 16:30:41 +0300

    qba-389: CourseDataXmlHelper 100% code test coverage

---
+++ PXQBA/Tests/Macmillan.PXQBA.Business.Commands.Tests/QuestionDataXmlParserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Macmillan.PXQBA.Business.Commands.Tests
+{
+    [TestClass]
+    public class QuestionDataXmlParserTest
+    {
+        [TestMethod]
+        public void ToSearchResultEntity_ResultDoc_SearchedByField()
+        {
+        }
+    }
+}
