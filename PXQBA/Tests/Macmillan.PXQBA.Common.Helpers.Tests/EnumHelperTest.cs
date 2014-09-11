838d6bef728c494b04672cb709d086d7422fef4c
dbazylev <d.bazylev@gmail.com>
2014-09-11 17:39:21 +0300

    + Add tests for Common.Helpers

--- PXQBA/Tests/Macmillan.PXQBA.Common.Helpers.Tests/EnumHelperTest.cs
+++ PXQBA/Tests/Macmillan.PXQBA.Common.Helpers.Tests/EnumHelperTest.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Macmillan.PXQBA.Business.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Macmillan.PXQBA.Common.Helpers.Tests
 {
     [TestClass]
-    class EnumHelperTest
+    public class EnumHelperTest
     {
-            [TestMethod]
-          public void F()
-          {
-              
-          }
+
+        [TestMethod]
+        public void Equals_QuestionStatus_True()
+        {
+            var result = EnumHelper.Equals("Deleted", QuestionStatus.Deleted);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Equals_WrongQuestionStatus_False()
+        {
+            var result = EnumHelper.Equals("Deleted", QuestionStatus.InProgress);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetEnumValues_NotEnums_Exception()
+        {
+            var values = EnumHelper.GetEnumValues(typeof(object));
+        }
     }
 }
